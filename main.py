fba42dfe8b258f021e6ae4976c276357a3876558
haden <git@haden.io>
2023-12-11 19:45:04 -0800

    add page hosting to main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
-def main():
-    pass
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return FileResponse('index.html')
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=6841)
