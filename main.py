abca4f234e041bda2715b5a3ca5a776d13a90264
haden <git@haden.io>
2023-12-11 20:20:24 -0800

    add main endpoint for the webpage

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 @app.get("/")
 def read_root():
-    return FileResponse('index.html')
+    return FileResponse('templates/index.html')
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=6841)
