7d73a9d57b2eb3d95e17ce5d6c53668c39abc505
haden <git@haden.io>
2024-01-20 04:01:15 -0800

    break out functionality to seperate imagedb file

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
-from typing import Optional
-import tkinter as tk
-from tkinter import filedialog
-from fastapi import FastAPI, Request
+import time
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-import os
+
+from image_db import ImageDB
+
+image_db_instance = ImageDB()
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def get_image_db_singleton():
+    return image_db_instance
 
 
 @app.get("/")
@@ -20,43 +25,40 @@ def read_root():
 
 
 @app.get("/search")
-async def search(request: Request):
+async def search(request: Request, imagedb: ImageDB = Depends(get_image_db_singleton)):
     term = request.query_params.get('term', '')
-    file_paths = search_files(term)  # Replace this with your actual function
+
+    start_time = time.time()
+    file_paths = imagedb.search_files(term)
+    print(f"searched in {time.time() - start_time} seconds")
+
     return JSONResponse(content=file_paths)
 
 
-def search_files(term):
-    print(f"Searching for {term}")
-
-    # Get a list of all files in the test_images directory
-    file_paths = os.listdir("test_images")
-
-    # Prepend the directory name to the file names
-    file_paths = [os.path.join("test_images", f) for f in file_paths]
-
-    # randomize the order of the files
-    import random
-    random.shuffle(file_paths)
-
-    return file_paths
-
-
-@app.get("/test_images/{filename}")
-async def serve_images(filename: str):
-    return FileResponse(f"test_images/{filename}")
+@app.get("/image/{filename:path}")
+async def serve_images(filename: str, imagedb: ImageDB = Depends(get_image_db_singleton)):
+    if not imagedb.validate_image(filename):
+        # fail
+        print(f"Image not found: {filename}")
+        return FileResponse('static/404.html')
+    print(f"Getting image: {filename}")
+    return FileResponse(f"/{filename}")
 
 
 @app.get("/open_directory_dialog")
-def open_directory_dialog():
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    folder_selected = filedialog.askdirectory()  # Open the folder dialog
-    root.destroy()  # Destroy the main window
+def open_directory_dialog(imagedb: ImageDB = Depends(get_image_db_singleton)):
+    directory = imagedb.get_directory()
 
-    print(f"Selected folder: {folder_selected}")
+    print(f"Selected folder: {directory}")
 
-    return JSONResponse(content={"folder_selected": folder_selected})
+    imagedb.index_directory(directory)
+
+    return JSONResponse(content={"folder_selected": directory})
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse('static/assets/favicon.png')
 
 
 if __name__ == "__main__":
